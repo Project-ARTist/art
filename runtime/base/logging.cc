5d6c927349ba75397f08914e338bd3cabcbcc0f2
Sebastian Weisgerber <sweisgerber.dev@gmail.com>
2018-05-16 16:44:24 +0200

    Artist Nougat 7.1: Removed ARTist as submodule (#10)
    * Removed artist as submodule
    
    * Added compiler/optimizing/artist to gitignore
    
    * Fixed broken CMakeLists.txt
    
    * Simplified the rewritten application logtag.
    
    * Removed ARTist as submodule: Managing artist via local_manifest/
    
    - See new local_manifests/ at:
    
      https://artist.cispa.saarland/build-setup/
    
    * Updated gitignore

--- runtime/base/logging.cc
+++ runtime/base/logging.cc
@@ -63,8 +63,12 @@ const char* GetCmdLine() {
   return (gCmdLine.get() != nullptr) ? gCmdLine->c_str() : nullptr;
 }
 
+// const char* ProgramInvocationName() {
+//  return (gProgramInvocationName.get() != nullptr) ? gProgramInvocationName->c_str() : "art";
+// }
+
 const char* ProgramInvocationName() {
-  return (gProgramInvocationName.get() != nullptr) ? gProgramInvocationName->c_str() : "art";
+  return "dex2artist";
 }
 
 // const char* ProgramInvocationShortName() {
@@ -73,8 +77,7 @@ const char* ProgramInvocationName() {
 // }
 
 const char* ProgramInvocationShortName() {
-  return (gProgramInvocationShortName.get() != nullptr) ? "dex2artist"
-                                                        : "dex2artist";
+  return "dex2artist";
 }
 
 void InitLogging(char* argv[]) {
